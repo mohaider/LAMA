b199ce7edcb2ed76d62055005200c1eae5c55fd8
a m <cookies.aaaaaaaaaaaaaaah@gmail.com>
2015-01-24 02:33:32 -0500

    Added place holder background and characters (see Crowd). Script (CrowdMovement.cs in Crowd obj) makes the crowd move across the map.

---
+++ Assets/CrowdMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CrowdMovement : MonoBehaviour {
+    [SerializeField]
+    Camera cam;
+    bool triggerEvent = false;
+    // Use this for initialization
+	void Start () {
+	        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (triggerEvent == false)
+        {
+            cam.transform.position += new Vector3(0.1f, 0, 0);
+            transform.position += new Vector3(0.1f, 0, 0);
+        }
+	}
+
+    public void setTriggerEvent(bool newTriggerEvent) {
+        triggerEvent = newTriggerEvent;
+    }
+}
